1c073c7c64d0109bc84e89f6518b0ef06f357114
LucasGarci <lucasgarcifer@gmail.com>
2023-02-19 12:40:36 +0100

    Litle refactor

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
 import React, { Suspense } from 'react';
 import './App.css';
-import ErrorBoundary from './features/errorBoundary/ErrorBoundary'
+import ErrorBoundary from './app/components/errorBoundary/ErrorBoundary'
 
 //Nota: Implementado un lazy laoding, ideal para reducir el tamaño de los bundles de producción y reducir tiempos de carga.
-const Metrics = React.lazy(() => import('./features/metrics/Metrics'));
+const Metrics = React.lazy(() => import('./app/components/metrics/Metrics'));
 
 function App() {
   // Nota: Usando el wrap con el método componentDidCatch podemos mostrar un placeholder en 
--- front-end/src/app/components/metrics/Metrics.js
+++ front-end/src/app/components/metrics/Metrics.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { /*LOAD_DB_URL,*/ POST_TEST_URL } from "../../app/constants.js"
+import { /*LOAD_DB_URL,*/ POST_TEST_URL } from "../../constants.js"
 import { createRandomMetric } from "./metricUtils"
 import { useSelector } from 'react-redux'
 import MetricsTable from "./MetricsTable"
@@ -8,13 +8,11 @@ import Counter from "../counter/Counter"
 import { MyButton } from "../../styled/buttons"
 
 //TODO: Set and alert for no server response
-const updateMetrics = (metrics, callback) => {
+const sendMetrics = (metrics, callback) => {
   // En este caso he optado por lo funciona frente a lo bonito
   // Lo ideal sería desactivar el botón de enviar mientras no hubiera al menos 4 métricas 
   // Y crear un flujo para el usuario donde no pueda equivocarse
   if (metrics.length < 4) return alert("Crea al menos 4 métricas")
-
-  console.log("Fetch start")
 
   const headers = new Headers()
   headers.append('Content-Type', 'application/json')
@@ -24,6 +22,7 @@ const updateMetrics = (metrics, callback) => {
     headers,
     body: stringData,
   }
+
   return fetch(POST_TEST_URL, requestOptions)
     .then((response) => {
       if (!response.ok) {
@@ -48,20 +47,18 @@ const Metrics = () => {
   const count = useSelector((state) => state.counter.value)
   const [metrics, setMetrics] = useState([]);
 
-  const addMetric = (metric) => {
-    setMetrics([...metrics, metric]);
-  }
-
-  //Nota: updateMetrics es llamada 2 veces por el StricMode, esto solo afecta al desarrollo, no a producción.
+  //TODO: Make a loadMetrics() to load al metrics from db on componentDidMount
+  //Nota: loadMetrics es llamada 2 veces por el StricMode, esto solo afecta al desarrollo, no a producción.
   //esto ocurre como una ayuda para mantener los componentes puros, hablando del paradigma de la programación funcional
   useEffect(() => {
-    //updateMetrics()
+    //loadMetrics()
     return () => {
     }
   }, [])
 
   const addRandomMetric = () => {
-    addMetric(createRandomMetric())
+    const newMetric = createRandomMetric()
+    setMetrics([...metrics, newMetric])
   }
 
   return (
@@ -73,7 +70,7 @@ const Metrics = () => {
       <div>
         <Counter />
         <MyButton name="Añadir métrica" onClick={addRandomMetric}>Añadir {count} métrica{count > 1 ? "s" : null} </MyButton>
-        <MyButton name="Enviar métricas" onClick={() => updateMetrics(metrics, setMetrics)}>Enviar métricas  </MyButton>
+        <MyButton name="Enviar métricas" onClick={() => sendMetrics(metrics, setMetrics)}>Enviar métricas  </MyButton>
       </div>
       <MetricsTable metrics={metrics} />
     </>
