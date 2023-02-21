d2c9a9aa79c3eb954c46ede245042240d9d70a16
LucasGarci <lucasgarcifer@gmail.com>
2023-02-22 00:22:52 +0100

    Update test messages

--- front-end/src/app/components/scatterChart/scatterChart.test.js
+++ front-end/src/app/components/scatterChart/scatterChart.test.js
@@ -8,10 +8,10 @@ describe("ScatterChart", () => {
         render(<ScatterChart metrics={[createRandomMetric()]} />)
         expect(screen.getByRole('img')).toBeInTheDocument()
     })
-    it('should throw an error', () => {
+    it('should throw error Metric atribute array is mandatory', () => {
         expect(() => render(<ScatterChart />)).toThrow("Metric atribute array is mandatory");
     })
-    it('should throw an error', () => {
+    it('should throw error Bad metric structure', () => {
         expect(() => render(<ScatterChart metrics={[{}]} />)).toThrow("Bad metric structure");
     })
 })
